32be1c08a2ee18c4ca45b7b6410eb070906518b8
Sai Kiran Chavan <csaikiran97@gmail.com>
2019-09-24 19:26:04 +0530

    express add commit

---
+++ Mean_project_express/api/routes/mentor-register.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const Mentor=require('../../model/schema')
+
+module.exports=function(router){
+    router.get('/mentorregister/:name',function(req,res){
+        
+        v=req.params.name;
+
+        Mentor.find({"Mentorname":v},function(err,mentorData){
+            if(err)return console.log(err);
+            res.json(mentorData);
+        });
+    })
+
+    router.post('/mentorregister',function(req,res){
+        var mentor=req.body;
+        if(!mentor.Mentorname||!mentor.Email||
+            !mentor.Phoneno||
+            !mentor.Starttime||
+            !mentor.Endtime||
+            !mentor.technologies||
+            !mentor.Linked||
+            !mentor.Password){
+                res.json('show_message', {
+                    message: "Sorry, you provided worng info", type: "error"});
+        }
+        else{
+            let note=new Mentor(req.body)
+            note.save(function(err,note){
+                if(err){
+                    return res.status(400).json(err);
+                }
+                res.status(200).json(note)
+            })
+        }
+    })
+}
---
+++ Mean_project_express/model/schema.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+
+const UserSchema=new mongoose.Schema({
+    Username:{type:String},
+    Email:{type:String},
+    Phoneno:{type:Number},
+    Password:{type:String}  
+})
+
+const mentorSchema=new mongoose.Schema({
+    Mentorname:{type:String},
+    Email:{type:String},
+    Phoneno:{type:Number},
+    Starttime:{type:Date},
+    Endtime:{type:Date},
+    technologies:{type:Array},
+    Linked:{type:String},
+    Password:{type:String}
+})
+
+module.exports=mongoose.model('User',UserSchema);
+module.exports=mongoose.model('Mentor',mentorSchema);
